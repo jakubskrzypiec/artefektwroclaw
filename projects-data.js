22f67a436448a1aa497648e4b31354cc83a55b20
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-08-31 19:22:09 +0200

    Add files via upload

--- projects-data.js
+++ projects-data.js
@@ -1,324 +1,324 @@
 window.ARTEFEKT_PROJECTS = [
   {
-    "img": "assets/projects/project-01.jpg",
+    "img": "project-01.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "01"
   },
   {
-    "img": "assets/projects/project-02.jpg",
+    "img": "project-02.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "02"
   },
   {
-    "img": "assets/projects/project-03.jpg",
+    "img": "project-03.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "03"
   },
   {
-    "img": "assets/projects/project-04.jpg",
+    "img": "project-04.jpg",
     "title": "Pokój dziecięcy",
     "category": "dzieci",
     "number": "04"
   },
   {
-    "img": "assets/projects/project-05.jpg",
+    "img": "project-05.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "05"
   },
   {
-    "img": "assets/projects/project-06.jpg",
+    "img": "project-06.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "06"
   },
   {
-    "img": "assets/projects/project-07.jpg",
+    "img": "project-07.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "07"
   },
   {
-    "img": "assets/projects/project-08.jpg",
+    "img": "project-08.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "08"
   },
   {
-    "img": "assets/projects/project-09.jpg",
+    "img": "project-09.jpg",
     "title": "Kuchnia",
     "category": "kuchnia",
     "number": "09"
   },
   {
-    "img": "assets/projects/project-10.jpg",
+    "img": "project-10.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "10"
   },
   {
-    "img": "assets/projects/project-11.jpg",
+    "img": "project-11.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "11"
   },
   {
-    "img": "assets/projects/project-12.jpg",
+    "img": "project-12.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "12"
   },
   {
-    "img": "assets/projects/project-13.jpg",
+    "img": "project-13.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "13"
   },
   {
-    "img": "assets/projects/project-14.jpg",
+    "img": "project-14.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "14"
   },
   {
-    "img": "assets/projects/project-15.jpg",
+    "img": "project-15.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "15"
   },
   {
-    "img": "assets/projects/project-16.jpg",
+    "img": "project-16.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "16"
   },
   {
-    "img": "assets/projects/project-17.jpg",
+    "img": "project-17.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "17"
   },
   {
-    "img": "assets/projects/project-18.jpg",
+    "img": "project-18.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "18"
   },
   {
-    "img": "assets/projects/project-19.jpg",
+    "img": "project-19.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "19"
   },
   {
-    "img": "assets/projects/project-20.jpg",
+    "img": "project-20.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "20"
   },
   {
-    "img": "assets/projects/project-21.jpg",
+    "img": "project-21.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "21"
   },
   {
-    "img": "assets/projects/project-22.jpg",
+    "img": "project-22.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "22"
   },
   {
-    "img": "assets/projects/project-23.jpg",
+    "img": "project-23.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "23"
   },
   {
-    "img": "assets/projects/project-24.jpg",
+    "img": "project-24.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "24"
   },
   {
-    "img": "assets/projects/project-25.jpg",
+    "img": "project-25.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "25"
   },
   {
-    "img": "assets/projects/project-26.jpg",
+    "img": "project-26.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "26"
   },
   {
-    "img": "assets/projects/project-27.jpg",
+    "img": "project-27.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "27"
   },
   {
-    "img": "assets/projects/project-28.jpg",
+    "img": "project-28.jpg",
     "title": "Pokój dziecięcy",
     "category": "dzieci",
     "number": "28"
   },
   {
-    "img": "assets/projects/project-29.jpg",
+    "img": "project-29.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "29"
   },
   {
-    "img": "assets/projects/project-30.jpg",
+    "img": "project-30.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "30"
   },
   {
-    "img": "assets/projects/project-31.jpg",
+    "img": "project-31.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "31"
   },
   {
-    "img": "assets/projects/project-32.jpg",
+    "img": "project-32.jpg",
     "title": "Kuchnia",
     "category": "kuchnia",
     "number": "32"
   },
   {
-    "img": "assets/projects/project-33.jpg",
+    "img": "project-33.jpg",
     "title": "Kuchnia",
     "category": "kuchnia",
     "number": "33"
   },
   {
-    "img": "assets/projects/project-34.jpg",
+    "img": "project-34.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "34"
   },
   {
-    "img": "assets/projects/project-35.jpg",
+    "img": "project-35.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "35"
   },
   {
-    "img": "assets/projects/project-36.jpg",
+    "img": "project-36.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "36"
   },
   {
-    "img": "assets/projects/project-37.jpg",
+    "img": "project-37.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "37"
   },
   {
-    "img": "assets/projects/project-38.jpg",
+    "img": "project-38.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "38"
   },
   {
-    "img": "assets/projects/project-39.jpg",
+    "img": "project-39.jpg",
     "title": "Kuchnia",
     "category": "kuchnia",
     "number": "39"
   },
   {
-    "img": "assets/projects/project-40.jpg",
+    "img": "project-40.jpg",
     "title": "Kuchnia",
     "category": "kuchnia",
     "number": "40"
   },
   {
-    "img": "assets/projects/project-41.jpg",
+    "img": "project-41.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "41"
   },
   {
-    "img": "assets/projects/project-42.jpg",
+    "img": "project-42.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "42"
   },
   {
-    "img": "assets/projects/project-43.jpg",
+    "img": "project-43.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "43"
   },
   {
-    "img": "assets/projects/project-44.jpg",
+    "img": "project-44.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "44"
   },
   {
-    "img": "assets/projects/project-45.jpg",
+    "img": "project-45.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "45"
   },
   {
-    "img": "assets/projects/project-46.jpg",
+    "img": "project-46.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "46"
   },
   {
-    "img": "assets/projects/project-47.jpg",
+    "img": "project-47.jpg",
     "title": "Łazienka",
     "category": "lazienka",
     "number": "47"
   },
   {
-    "img": "assets/projects/project-48.jpg",
+    "img": "project-48.jpg",
     "title": "Pokój dziecięcy",
     "category": "dzieci",
     "number": "48"
   },
   {
-    "img": "assets/projects/project-49.jpg",
+    "img": "project-49.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "49"
   },
   {
-    "img": "assets/projects/project-50.jpg",
+    "img": "project-50.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "50"
   },
   {
-    "img": "assets/projects/project-51.jpg",
+    "img": "project-51.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "51"
   },
   {
-    "img": "assets/projects/project-52.jpg",
+    "img": "project-52.jpg",
     "title": "Strefa dzienna",
     "category": "salon",
     "number": "52"
   },
   {
-    "img": "assets/projects/project-53.jpg",
+    "img": "project-53.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "53"
   },
   {
-    "img": "assets/projects/project-54.jpg",
+    "img": "project-54.jpg",
     "title": "Sypialnia",
     "category": "sypialnia",
     "number": "54"
